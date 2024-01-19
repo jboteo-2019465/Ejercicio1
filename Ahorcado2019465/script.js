c0bbcefc44f4c8843142223a5a146d1b001b03d7
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-01-19 08:58:04 -0600

    Segundo Push

--- Ahorcado2019465/script.js
+++ Ahorcado2019465/script.js
@@ -1,15 +1,25 @@
+
+
 function draw(){
     var canvas = document.getElementById("canvas");
-    if (canvas.getContext("2d")){
+    
         const ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(75, 75, 50, 0, Math.PI *2, true);
+        ctx.moveTo(600, 0)
+        ctx.stroke();
+        
 
 
-    }
+
 
 }
 
 function letras(){
-    
+    for (let i = 65; i < 91; i++) {
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText = String.fromCharCode(i);}
 
 }
 
