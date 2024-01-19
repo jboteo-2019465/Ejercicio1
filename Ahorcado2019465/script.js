e29d26b7618b7e844ff7eda982b500d0bce60f2c
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-01-19 09:17:49 -0600

    Push Tres

--- Ahorcado2019465/script.js
+++ Ahorcado2019465/script.js
@@ -1,19 +1,26 @@
 
 
-function draw(){
+function cabeza(){
     var canvas = document.getElementById("canvas");
-    
         const ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.arc(75, 75, 50, 0, Math.PI *2, true);
-        ctx.moveTo(600, 0)
+        ctx.moveTo(200, 200);
         ctx.stroke();
+}
+function cuerpo(){
+    ctx.beginPath();
+    ctx.lineTo(75, 75);
+    ctx.moveTo(200, 200);
+    ctx.sroke();
+
+}
         
 
 
 
 
-}
+
 
 function letras(){
     for (let i = 65; i < 91; i++) {
@@ -24,3 +31,5 @@ function letras(){
 }
 
 const palabras = ['html', 'mesa']
+cabeza();
+cuerpo();
