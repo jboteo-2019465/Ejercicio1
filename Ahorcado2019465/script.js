85d6ff205c0279bcd11fe3e75f5986e76009263f
jboteo-2019465 <jboteo-2019465kinal.edu.gt>
2024-01-19 07:51:00 -0600

    Primer Push

---
+++ Ahorcado2019465/script.js
@@ -0,0 +1,16 @@
+function draw(){
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext("2d")){
+        const ctx = canvas.getContext("2d");
+
+
+    }
+
+}
+
+function letras(){
+    
+
+}
+
+const palabras = ['html', 'mesa']
